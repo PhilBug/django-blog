7922baa4495bb33d47b98bb40be14f180c222b86
fbugaj <fbugaj@match-trade.com>
2021-01-24 19:58:44 +0100

    make date field obligatory

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
-
 from posts.models import Post
+
 
 def index(request):
     posts = Post.objects.all()
--- posts/models.py
+++ posts/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     photo_description = models.CharField(max_length=200)
     content = models.TextField()
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    date_added = models.DateTimeField(default=datetime.now, blank=True)
+    date_added = models.DateTimeField(default=datetime.now, blank=False)
 
     def __str__(self):
         return self.title
